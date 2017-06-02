415f567dfef5a700eb5f04049419f604149bf97f
Dave Hausler <dave.hausler@gmail.com>
2017-06-02 18:44:27 -0400

    Created Components, section 42 & 43

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './header/recipe-book/recipe-list/recipe-item/recipe-item.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
