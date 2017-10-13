4ebc2c88cfd231dc73e94268e09fac105f59fd7a
Dave Hausler <dave.hausler@gmail.com>
2017-10-13 15:18:46 -0400

    Implemented Recipe and Shopping-List services, section 104

---
+++ src/app/header/recipe-book/recipe.service.ts
@@ -0,0 +1,13 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://media2.sailusfood.com/wp-content/uploads/2016/02/cheese-corn-balls-recipe.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'http://media2.sailusfood.com/wp-content/uploads/2016/02/cheese-corn-balls-recipe.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
