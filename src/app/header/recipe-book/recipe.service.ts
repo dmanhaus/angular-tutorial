6f9994aaa4fd92993fdceced1f647c733a6385db
Dave Hausler <dave.hausler@gmail.com>
2017-10-15 12:50:03 -0400

    Added ingredients to recipes, section 109

--- src/app/header/recipe-book/recipe.service.ts
+++ src/app/header/recipe-book/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
@@ -6,8 +7,22 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://media2.sailusfood.com/wp-content/uploads/2016/02/cheese-corn-balls-recipe.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'http://media2.sailusfood.com/wp-content/uploads/2016/02/cheese-corn-balls-recipe.jpg')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20)
+    ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else do you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+    [
+      new Ingredient('Buns', 2),
+      new Ingredient('Meat', 1)
+    ])
   ];
 
   getRecipes() {
